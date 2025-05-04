96b4ac24d0e2c939d7fdc9466f707dfceacab20f
PraveenNarwal <praveennarwal5@gmail.com>
2025-05-04 15:05:48 +0530

    customization not completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <p className="text-rose-500"> 
         Hello World shadcn ui
         </p>
+        <Button variant="elevated">helloworls</Button>
     </div>
   );
 }
